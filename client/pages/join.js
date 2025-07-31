2172dc5663672109e32d75e5be068d4a6f8603e4
nnapp-admin <iown.nutrinationapp@gmail.com>
2025-08-01 00:37:04 +0530

    ok

--- client/pages/join.js
+++ client/pages/join.js
@@ -252,7 +252,7 @@ export default function RegisterPage() {
           dataToSend.startupLogo = await convertToBase64(dataToSend.startupLogo);
         }
 
-        const response = await fetch('http://localhost:3001/api/register', {
+        const response = await fetch('https://foundercultbackend.onrender.com/api/register', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
